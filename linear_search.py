83c34446f180575a6f0d74b05c3155ed2b3f94ba
KATE_FISCHER <KATME2006@GMAIL.COM>
2023-10-08 10:33:43 -0500

    Linear Algorithms with Tests Passed

--- linear_search.py
+++ linear_search.py
@@ -1,7 +1,18 @@
 def linear_search(value_to_find, array_to_search_through):
-    # your code here
-    pass
+    
+    for i in array_to_search_through:
+        if i == value_to_find:
+            return array_to_search_through.index(i)
+
+    
 
 def linear_search_global(value_to_find, array_to_search_through):
-    # your code here
-    pass
+    index_arr =[]
+    index_counter =0
+    for i in array_to_search_through:
+        if i == value_to_find:
+            index_arr.append(index_counter)
+        index_counter += 1
+    return index_arr
+
+
